a65b3f9130db4b10217965444e61086717737750
EvanRPavone <EvanRPavone@gmail.com>
2025-06-25 11:41:01 -0400

    restarting frontend

--- frontend/src/utils/themeUtil.ts
+++
@@ -1,9 +0,0 @@
-export const parseCustomization = (config: { [key: string]: string }) => {
-    return {
-        primary: config["Primary Color"] || "#9860ff",
-        secondary: config["Secondary Color"] || "#9860ff",
-        font: config["Font"] || "Inter",
-        themeStyle: config["Theme Style"] || "dark",
-        layout: config["Layout"] || "grid",
-    };
-};
